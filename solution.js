666cac07f2870adc13bdbaaec1025953a6952e92
YanevDimo <dimocz@gmail.com>
2023-11-06 13:42:04 +0100

    add function earchFormData

--- solution.js
+++ solution.js
@@ -21,3 +21,18 @@ document.querySelector('#new-reservation').addEventListener('click', (e) => clea
 function cleanData(e) {
     changeContent('search-form-content');
 }
+function searchFormData(e) {
+    e.preventDefault();
+    const data = e.target.parentElement;
+    const checkIn = data.querySelector('#check-in').value;
+    const checkOut = data.querySelector('#check-out').value;
+    const people = data.querySelector('#people').value;
+    if (checkIn != '' && checkOut != '' && people != '' &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        console.log(reservation);
+        changeContent('search-result-form-content');
+    }
+}
